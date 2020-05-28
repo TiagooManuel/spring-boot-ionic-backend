1f0d813b26b864713488bc6f3b173ce3904dcb29
Tiago Manuel <tiagoomanuel@hotmail.com>
2020-05-28 15:35:48 +0100

    Projeto criado

---
+++ src/main/java/com/udemy/udemySpringHibernate/UdemySpringHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.udemySpringHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/udemySpringHibernate/UdemySpringHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.udemySpringHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
