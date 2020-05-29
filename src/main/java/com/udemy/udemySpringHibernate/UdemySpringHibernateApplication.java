25d828e2664274f60c51061f23eeadf46aadaf47
Tiago Manuel <tiagoomanuel@hotmail.com>
2020-05-29 16:27:52 +0100

    Criando operação de instanciação

--- src/main/java/com/udemy/udemySpringHibernate/UdemySpringHibernateApplication.java
+++ src/main/java/com/udemy/udemySpringHibernate/UdemySpringHibernateApplication.java
@@ -1,13 +1,37 @@
 package com.udemy.udemySpringHibernate;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class UdemySpringHibernateApplication {
+import com.udemy.udemySpringHibernate.domain.Categoria;
+import com.udemy.udemySpringHibernate.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class UdemySpringHibernateApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UdemySpringHibernateApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		//para guardar no BD, usa-se o repository que é o responsavel por comunicar com a BD
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
+		
 	}
 
 }
