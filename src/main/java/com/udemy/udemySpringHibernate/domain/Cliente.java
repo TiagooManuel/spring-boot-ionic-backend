b6527112a6c338a39b2b1c9a1c73fa2a0704cb99
Tiago Manuel <tiagoomanuel@hotmail.com>
2020-06-02 10:59:30 +0100

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/udemy/udemySpringHibernate/domain/Cliente.java
+++ src/main/java/com/udemy/udemySpringHibernate/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.udemy.udemySpringHibernate.domain.enumns.TipoCliente;
 
 @Entity
@@ -31,6 +32,7 @@ public class Cliente implements Serializable {
 	private Integer tipo;
 	
 	@OneToMany(mappedBy="cliente")
+	@JsonManagedReference
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	//coleção (conjunto de strings)
