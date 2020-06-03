34c047e377b4c63f114b7d9f06cdd85e89053009
Tiago Manuel <tiagoomanuel@hotmail.com>
2020-06-03 17:21:01 +0100

    Listando todas as Categorias com DTO

--- src/main/java/com/udemy/udemySpringHibernate/services/CategoriaService.java
+++ src/main/java/com/udemy/udemySpringHibernate/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.udemy.udemySpringHibernate.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,4 +48,10 @@ public class CategoriaService {
 		}
 		
 	}
+	
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+	
 }
