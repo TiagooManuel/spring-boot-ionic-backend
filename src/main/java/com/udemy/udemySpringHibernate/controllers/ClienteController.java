98b84302e0805edf2ded28e2454ff2d37c03a725
Tiago Manuel <tiagoomanuel@hotmail.com>
2020-06-15 11:06:09 +0100

    Inserindo novo cliente com POST

--- src/main/java/com/udemy/udemySpringHibernate/controllers/ClienteController.java
+++ src/main/java/com/udemy/udemySpringHibernate/controllers/ClienteController.java
@@ -1,5 +1,6 @@
 package com.udemy.udemySpringHibernate.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,10 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.udemy.udemySpringHibernate.domain.Categoria;
 import com.udemy.udemySpringHibernate.domain.Cliente;
 import com.udemy.udemySpringHibernate.dto.ClienteDTO;
+import com.udemy.udemySpringHibernate.dto.ClienteNewDTO;
 import com.udemy.udemySpringHibernate.services.ClienteService;
 
 @RestController
@@ -34,6 +37,16 @@ public class ClienteController {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@RequestMapping(method = RequestMethod.POST)
+		public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+		//instanciar um cliente a partir de um ClienteDTO
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+			return ResponseEntity.created(uri).build();
+	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id) {
@@ -82,4 +95,6 @@ public class ClienteController {
 
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	
 }
--- src/main/java/com/udemy/udemySpringHibernate/services/ClienteService.java
+++ src/main/java/com/udemy/udemySpringHibernate/services/ClienteService.java
@@ -3,15 +3,23 @@ package com.udemy.udemySpringHibernate.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import com.udemy.udemySpringHibernate.domain.Cidade;
 import com.udemy.udemySpringHibernate.domain.Cliente;
+import com.udemy.udemySpringHibernate.domain.Endereco;
+import com.udemy.udemySpringHibernate.domain.enumns.TipoCliente;
 import com.udemy.udemySpringHibernate.dto.ClienteDTO;
+import com.udemy.udemySpringHibernate.dto.ClienteNewDTO;
+import com.udemy.udemySpringHibernate.repositories.CidadeRepository;
 import com.udemy.udemySpringHibernate.repositories.ClienteRepository;
+import com.udemy.udemySpringHibernate.repositories.EnderecoRepository;
 import com.udemy.udemySpringHibernate.services.exceptions.DataIntegrityException;
 import com.udemy.udemySpringHibernate.services.exceptions.ObjectNotFoundException;
 
@@ -20,11 +28,26 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	
@@ -35,20 +58,13 @@ public class ClienteService {
 		return repo.save(newObj);
 	}
 	
-	private void updateData(Cliente newObj, Cliente obj) {
-		newObj.setNome(obj.getNome());
-		newObj.setEmail(obj.getEmail());
-	}
-	
-
-	
-	
 	public void delete(Integer id) {
 		//verificar se o id existe
 		find(id);
 		try {
 			repo.deleteById(id);
-		} catch (DataIntegrityViolationException e) {
+		} 
+		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluír porque há entidades relacionadas");
 		}
 		
@@ -65,11 +81,33 @@ public class ClienteService {
 		return repo.findAll(pageRequest);
 	}
 	
+	
 	//a partir do DTO, construir um objeto Cliente
 	public Cliente fromDTO(ClienteDTO objDTO) {
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
 	}
 	
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 	
 	
 
